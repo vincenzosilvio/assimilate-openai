55ecd2a9ce5d9bb4eaae53aa39df656dd1c5c81c
Noah Gift <noah.gift@gmail.com>
2022-09-08 21:25:18 +0000

    adding working code generator web app for open ai

---
+++ codeGenWebApp.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from oalib.solutions import create_code
+
+st.title("Code Generator")
+st.write("Convert a comment into code in any language")
+
+language = st.text_input("Language", "python")
+text = st.text_area("Comment", "Calculate the mean distance between an array of points")
+
+if st.button("Generate Code"):
+    with st.spinner(text='In progress'):
+        code = create_code(text, language)
+        st.code(code)
