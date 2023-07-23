a95228bcbed547cf48d170cb33d212536c3667f7
Noah Gift <noah.gift@gmail.com>
2023-07-23 17:45:28 +0000

    adding documentation

--- chatbot/src/main.rs
+++ chatbot/src/main.rs
@@ -1,3 +1,21 @@
+/* 
+This project uses several libraries:
+- reqwest for making HTTP requests,
+- serde_json for handling JSON data,
+- std::io for handling input/output.
+
+It also uses the OpenAI API to interact with an AI model.
+
+The main function, run_chat_loop, runs a chat loop that:
+- Reads user input,
+- Appends it to the conversation,
+- Sends the conversation to the OpenAI API,
+- Receives the model's response,
+- Prints the response,
+- Repeats this process until the user types 'quit' or 'exit'.
+*/
+
+
 use chatbot::chatbot::run_chat_loop;
 use reqwest::Client;
 
